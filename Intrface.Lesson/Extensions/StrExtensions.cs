2726e0bc6db41ef2d262f8e2d7f919f62963171c
ssevilll <sevilsb@code.edu.az>
2025-11-01 14:31:33 +0400

    second

---
+++ Intrface.Lesson/Extensions/StrExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Intrface.Lesson.Extensions
+{
+    internal static class StrExtensions
+    {
+        public static int MatchCount(this string str, string substring)
+        {
+            if (string.IsNullOrEmpty(str) || string.IsNullOrEmpty(substring))
+            {
+                return 0;
+            }
+            int count = 0;
+            int index = 0;
+            while ((index = str.IndexOf(substring, index, StringComparison.Ordinal)) != -1)
+            {
+                count++;
+                index += substring.Length;
+            }
+            return count;
+        }
+    }
+}
--- Intrface.Lesson/Program.cs
+++ Intrface.Lesson/Program.cs
@@ -6,20 +6,26 @@ namespace Intrface.Lesson
     {
         static void Main(string[] args)
         {
-            int a = 3;
-            Console.WriteLine(a.IsEven());
-            Console.WriteLine(a.IsOdd());
+            #region Task1
+            //int a = 3;
+            //Console.WriteLine(a.IsEven());
+            //Console.WriteLine(a.IsOdd());
 
-            string str = "hello world1. this is c#.";
-            Console.WriteLine(str.IsContainsDigit());
-            Console.WriteLine(str.ToCapitalize());
-            var indexes = str.GetValueIndexes('o');
-            foreach (var index in indexes)
-            {
-                Console.WriteLine(index);
-            }
-            Console.WriteLine(str.GetFirstSentence());
-            Console.WriteLine(str.GetSecondWord());
+            //string str = "hello world1. this is c#.";
+            //Console.WriteLine(str.IsContainsDigit());
+            //Console.WriteLine(str.ToCapitalize());
+            //var indexes = str.GetValueIndexes('o');
+            //foreach (var index in indexes)
+            //{
+            //    Console.WriteLine(index);
+            //}
+            //Console.WriteLine(str.GetFirstSentence());
+            //Console.WriteLine(str.GetSecondWord());
+            #endregion
+
+            string test = "Salam Kamran. Necəsən Kamran";
+            string substring = "Kamran";
+            Console.WriteLine(test.MatchCount(substring));
         }
     }
 }
