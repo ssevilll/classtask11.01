8ab5fc3055502d73c8a9e07c9ef8a6f32deb732e
ssevilll <sevilsb@code.edu.az>
2025-11-01 14:28:22 +0400

    initial

---
+++ Intrface.Lesson/Extensions/ExtentionMethods.cs
@@ -0,0 +1,78 @@
+﻿namespace Intrface.Lesson.Extensions
+{
+    internal static class ExtentionMethods
+    {
+        public static bool IsOdd(this int number)
+        {
+            return number % 2 != 0;
+        }
+
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static bool IsContainsDigit(this string str)
+        {
+            foreach (char c in str)
+            {
+                if (char.IsDigit(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static string ToCapitalize(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+
+        public static int[] GetValueIndexes(this string str, char character)
+        {
+            int[] indexes = new int[str.Length];
+            int j = 0;
+            for (int i = 0; i <str.Length; i++)
+            {
+                if (str[i] == character)
+                {
+                    indexes[j++] = i;
+                }
+            }
+            if (j==0)
+            {
+                return [];
+            }
+            else
+            {
+                Array.Resize(ref indexes, j);
+            }
+            return indexes;
+        }
+
+        public static string GetFirstSentence(this string str)
+        {
+            int periodIndex = str.IndexOf('.');
+            if (periodIndex != -1)
+            {
+                return str.Substring(0, periodIndex + 1);
+            }
+            return str;
+        }
+
+        public static string GetSecondWord(this string str)
+        {
+            string[] words = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length >= 2)
+            {
+                return words[1];
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ Intrface.Lesson/Program.cs
@@ -0,0 +1,25 @@
+﻿using Intrface.Lesson.Extensions;
+
+namespace Intrface.Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            Console.WriteLine(a.IsEven());
+            Console.WriteLine(a.IsOdd());
+
+            string str = "hello world1. this is c#.";
+            Console.WriteLine(str.IsContainsDigit());
+            Console.WriteLine(str.ToCapitalize());
+            var indexes = str.GetValueIndexes('o');
+            foreach (var index in indexes)
+            {
+                Console.WriteLine(index);
+            }
+            Console.WriteLine(str.GetFirstSentence());
+            Console.WriteLine(str.GetSecondWord());
+        }
+    }
+}
